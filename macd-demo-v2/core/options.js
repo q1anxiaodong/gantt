65e4abe8a6a7a089e250f673e873cff328fcea22
qianxiaodong <1465382456@qq.com>
2023-03-27 14:51:39 +0800

    A5-761 feat 布林线策略图

--- macd-demo-v2/core/options.js
+++ macd-demo-v2/core/options.js
@@ -297,6 +297,17 @@ const getOption = (kData) => {
   };
 };
 
+const legendLabelMap = {
+  bulin: {
+    data: [{ name: "BOLL" }, { name: "MID" }, { name: "UP" }, { name: "LOW" }],
+  },
+};
+const legendLabelRichMap = {
+  BOLL: { fill: "rgba(0, 0, 0, 0.6)" },
+  MID: { fill: "#36f" },
+  UP: { fill: "#ff2436" },
+  LOW: { fill: "#07ab4b" },
+};
 //---------------------------------
 const getData = (data) => {
   return [{ originData: data }];
@@ -317,14 +328,20 @@ const getGrid = (index, scale) => {
   const originLeft = 10;
   const OriginRight = 10;
   // const top = isMainGrid ? 0 : "67%";
-  const bottom = isMainGrid ? "38%" : '5%';
+  const bottom = isMainGrid ? "38%" : "5%";
   return {
     left: originLeft * scale,
     // top,
     right: OriginRight * scale,
     bottom,
     height: originHeight * scale,
-    background: { show: false, borderEnable: 1, style: {fill: index === 0 ? 'rgba(255, 0, 0, 0.3)' : 'rgba(0, 255, 0, 0.3)'}}
+    background: {
+      show: false,
+      borderEnable: 1,
+      style: {
+        fill: index === 0 ? "rgba(255, 0, 0, 0.3)" : "rgba(0, 255, 0, 0.3)",
+      },
+    },
   };
 };
 
@@ -336,18 +353,18 @@ const getXAxis = (gridIndex, scale) => {
   );
   const axisLabelFormatter = (label, index, len) => {
     return `${label.slice(4, 6)}-${label.slice(6, 8)}`;
-  }
+  };
   const axisTickValues = (labelArr) => {
     const lastIndex = labelArr.length - 1;
     const percent = [0, 0.25, 0.5, 0.75, 1];
-    return percent.map(per => {
-        return labelArr[Math.floor(lastIndex * per)];
-    })
-  }
+    return percent.map((per) => {
+      return labelArr[Math.floor(lastIndex * per)];
+    });
+  };
   return {
     xOrY: "x",
-    position: 'bottom',
-    type: 'band',
+    position: "bottom",
+    type: "band",
     $dataIndex: 0,
     $gridIndex: gridIndex,
     dataKey: "t",
@@ -373,15 +390,14 @@ const getYAxis = (gridIndex, scale) => {
     const intervals = isMainYAxis ? [0, 0.25, 0.5, 0.75, 1] : [0, 0.5, 1];
     const extent = Math.abs(domain[0] - domain[1]);
     const start = Math.min(domain[0], domain[1]);
-    console.log('domain', domain);
-    return intervals.map(interval => start + interval * extent);
+    return intervals.map((interval) => start + interval * extent);
   };
   const labelFormatter = (originVal, index, len) => {
     const value = originVal.toFixed(2);
     if (index !== 0 && index !== len - 1) return `{middle|${value}}`;
-    if (!isMainYAxis) return '';
+    if (!isMainYAxis) return "";
     return value;
-  }
+  };
   return {
     position: "left",
     type: "linear",
@@ -396,11 +412,11 @@ const getYAxis = (gridIndex, scale) => {
       inside: true,
       inRange: true,
       formatter: labelFormatter,
-      style: { 
+      style: {
         fill: "rgba(0,0,0,0.6)",
         rich: {
-            middle: { textPadding: [12 * scale, 0, 0, 0] }
-        }
+          middle: { textPadding: [12 * scale, 0, 0, 0] },
+        },
       },
     },
     line: { show: false },
@@ -410,107 +426,185 @@ const getYAxis = (gridIndex, scale) => {
 };
 
 const getKLineSeries = (scale) => {
-    return {
-        type: 'hqbar',
-        hqbarType: 'kline',
-        compressThreshold: 60,
-        $dataIndex: 0,
-        $axisIndex: [0, 1],
-        hq: {
-            up: {fill: 'transparent',  lineWidth: 1 * scale},
-            down: {lineWidth: 1 * scale},
-            eq: {lineWidth: 1 * scale},
-        }
-    }
+  return {
+    type: "hqbar",
+    hqbarType: "kline",
+    compressThreshold: 60,
+    $dataIndex: 0,
+    $axisIndex: [0, 1],
+    hq: {
+      up: { fill: "transparent", lineWidth: 1 * scale },
+      down: { lineWidth: 1 * scale },
+      eq: { lineWidth: 1 * scale },
+    },
+  };
 };
 const getLineSeries = (name, dataKey, axisIndex, color, scale) => {
-    return {
-        name: name,
-        type: 'line',
-        aliasType: 'hqline',
-        dataKey: dataKey,
-        $dataIndex: 0,
-        $axisIndex: axisIndex,
-        line: {show: true, style: { normal: { stroke:  color, lineWidth: 1 * scale} } }
-    }
-}
+  return {
+    name: name,
+    type: "line",
+    aliasType: "hqline",
+    dataKey: dataKey,
+    $dataIndex: 0,
+    $axisIndex: axisIndex,
+    line: {
+      show: true,
+      style: { normal: { stroke: color, lineWidth: 1 * scale } },
+    },
+  };
+};
 const getBarSeries = (name, dataKey, axisIndex, upBarColor, downBarColor) => {
-    const itemStyleFun = (data) => {
-        // TODO: 后续删除条件中的常量
-        if (data[1] - 867651125 > 0) {return { fill: upBarColor }}
-        else {return { fill: downBarColor }}
-    };
-    return {
-        name: name,
-        type: 'bar',
-        $dataIndex: 0,
-        $axisIndex: axisIndex,
-        dataKey: dataKey,
-        itemStyle: {
-            normal: itemStyleFun
-        }
-    };
+  const itemStyleFun = (data) => {
+    // TODO: 后续删除条件中的常量
+    if (data[1] - 867651125 > 0) {
+      return { fill: upBarColor };
+    } else {
+      return { fill: downBarColor };
+    }
+  };
+  return {
+    name: name,
+    type: "bar",
+    $dataIndex: 0,
+    $axisIndex: axisIndex,
+    dataKey: dataKey,
+    itemStyle: {
+      normal: itemStyleFun,
+    },
+  };
 };
 const getLegend = (data, scale) => {
-    const legendFormatter = (label, params) => {
-        console.log('args', label, params);
-        return `{label|${label}}`;
-    }
-    return {
-        show: true,
-        top: '65%',
-        padding: 0,
-        addLine: false,
-        align: 'left',
-        horizontalGap: 0,
-        innerGap: 5,
-        // symbol: { type: 'none', size: 10 * scale },
-        formatter: legendFormatter,
-        textStyle: {
-            fontSize: 12 * scale,
-            rich: {
-                // label: { textVerticalAlign: 'middle' }
-            }
-        },
-        data: data
-    }
-}
+  const legendFormatter = (label, params) => {
+    return `{${label}|${label}}`;
+  };
+  return {
+    show: true,
+    top: "65%",
+    left: 10 * scale,
+    addLine: false,
+    horizontalGap: 40 * scale,
+    // innerGap: 10 * scale,
+    symbol: { type: 'none', size: 10 * scale },
+    formatter: legendFormatter,
+    textStyle: {
+      fontSize: 12 * scale,
+      rich: legendLabelRichMap,
+    },
+    data: data,
+  };
+};
 const getAxisPointer = (orient, axisIndex, gap, scale) => {
-    const isVertical = orient === 'vertical';
-    const axisPointerFormatter = (value) => {
-       return isVertical ? `${value.slice(4, 6)}-${value.slice(6, 8)}` : value.toFixed(2);
-    }
-    return {
-        $axisIndex: axisIndex,
-        line: {
-            style: {
-                stroke: '#858585',
-                lineWidth: 1 * scale,
-            }
-        },
-        label: {
-            gap: gap,
-            inside: true,
-            inRange: true,
-            formatter: axisPointerFormatter,
-            style: {
-                fill: '#fff',
-                textPadding: [3, 4],
-                textLineHeight: 12 * scale,
-                textBorderRadius: 4,
-                textBackgroundColor: '#3366ff'
-            }
-        }
-    }
-}
+  const isVertical = orient === "vertical";
+  const axisPointerFormatter = (value) => {
+    return isVertical
+      ? `${value.slice(4, 6)}-${value.slice(6, 8)}`
+      : value.toFixed(2);
+  };
+  return {
+    $axisIndex: axisIndex,
+    line: {
+      style: {
+        stroke: "#858585",
+        lineWidth: 1 * scale,
+      },
+    },
+    label: {
+      gap: gap,
+      inside: true,
+      inRange: true,
+      formatter: axisPointerFormatter,
+      style: {
+        fill: "#fff",
+        textPadding: [3, 4],
+        textLineHeight: 12 * scale,
+        textBorderRadius: 4,
+        textBackgroundColor: "#3366ff",
+      },
+    },
+  };
+};
+const getMarkArea = (axisIndex, seriesIndex, [start, end], isContainShape, scale) => {
+  const startXFormatter = (x, bandWidth) => {
+    const lineWidth = 1 * scale;
+    return x - bandWidth - lineWidth;
+  };
+  const endXFormatter = (x, bandWidth) => {
+    const lineWidth = 1 * scale;
+    return x + bandWidth + lineWidth;
+    ;
+  }
+  return {
+    $axisIndex: axisIndex,
+    $seriesIndex: seriesIndex,
+    xIndex: start,
+    areaStyle: {
+      normal: {
+        fill: "transparent",
+        stroke: "#36f",
+        lineDash: [20, 10],
+        lineWidth: 1 * scale,
+      },
+    },
+    data: [
+      {
+        points: [
+          { xIndex: start, y: "top", xFormatter: isContainShape ? startXFormatter : undefined },
+          { xIndex: end, y: "bottom", xFormatter: isContainShape ? endXFormatter : undefined },
+        ],
+      },
+    ],
+  };
+};
 //---------------------------------
 
+// 自定义系列
+//******************************** */
+const getFlagSeries = (name, axisIndex, xIndex, content, scale) => {
+  const renderFlag = function (model, globalModel, global) {
+    const xIndex = model.get("xIndex");
+    const yAxisIndex = axisIndex[1];
+    const axisModel = globalModel.getComponent('axis');
+    const xScale = axisModel[0].scale;
+    const YScale = axisModel[yAxisIndex].scale;
+    const extent = axisModel[yAxisIndex].domain
+    let flagAttr = [];
+    flagAttr.push({
+      shape: {
+        x: xScale(axisModel[0].domain[xIndex]),
+        y: YScale(extent[1]) + 12 * scale,
+      },
+      style: {
+        fontSize: 12 * scale,
+        textPadding: 5 * scale,
+        textBorderRadius: 2 * scale,
+        textOffset: [-20, 0],
+        textAlign: 'right',
+        textBackgroundColor: '#36f',
+        textFill: '#fff',
+        text: content
+      }
+    });
+    this.setShapeGroup('flag', D3Charts.graphic.Rect, flagAttr);
+  };
+  return {
+    type: "custom",
+    name: name,
+    $dataIndex: 0,
+    $axisIndex: axisIndex,
+    xIndex: xIndex,
+    view: {
+      render: renderFlag,
+    },
+  };
+};
+//******************************** */
 class OptionManager {
-  constructor(scale, chartName, data) {
+  constructor(scale, chartName, data, flagContent) {
     this.option = null;
     this.scale = scale;
     this.chartName = chartName;
     this.data = data;
+    this.flagContent = flagContent;
   }
 
   getOption() {
@@ -530,25 +624,36 @@ class OptionManager {
 
   getBulinChartOption() {
     // 这里是bulin图表的option逻辑
-    const { scale, data } = this;
+    const { scale, data, flagContent } = this;
+    if (!data) {
+      console.trace('what');
+    }
+    const dataLen = this.data.length;
     return {
+      scale: scale,
       data: getData(data),
       textStyle: getTextStyle(scale),
       grid: [getGrid(0, scale), getGrid(1, scale)],
       axis: [getXAxis([1, 0], scale), getYAxis(0, scale), getYAxis(1, scale)],
-      axisPointer: [ getAxisPointer('vertical', 0, -48, scale), getAxisPointer('horizontal', [1, 2], -4, scale) ],
-      legend: getLegend(['up', 'mid', 'low', 'boll'], scale),
+      axisPointer: [
+        getAxisPointer("vertical", 0, -48, scale),
+        getAxisPointer("horizontal", [1, 2], -4, scale),
+      ],
+      legend: getLegend(legendLabelMap["bulin"].data, scale),
       series: [
         getKLineSeries(scale),
         // up线
-        getLineSeries('up', 'a', [0, 2], '#ff2436', scale),
+        getLineSeries("UP", "line1", [0, 2], "#ff2436", scale),
         // mid线
-        getLineSeries('mid', 'o', [0, 2], '#3366ff', scale),
+        getLineSeries("MID", "line2", [0, 2], "#3366ff", scale),
         // low线
-        getLineSeries('low', 'i', [0, 2], '#07ab4b', scale),
+        getLineSeries("LOW", "line3", [0, 2], "#07ab4b", scale),
         // 下方的柱子
-        getBarSeries('boll', 'o', [0, 2], '#ff2436', '#07ab4b', )
+        getBarSeries("BOLL", "o", [0, 2], "#ff2436", "#07ab4b"),
+        getFlagSeries("klineFlag", [0, 1], dataLen - 1, flagContent[0].label, scale),
+        getFlagSeries("barFlag", [0, 2], dataLen - 1, flagContent[1].label, scale),
       ],
+      markArea: [getMarkArea([0, 1], 0, [dataLen - 1, dataLen - 1], true, scale), getMarkArea([0, 2], 1, [dataLen - 1, dataLen - 1], true, scale)],
     };
   }
 
@@ -584,5 +689,4 @@ class OptionManager {
 }
 
 // test
-const optionManager = new OptionManager(1.2, "bulin");
-console.log(optionManager.getOption());
+// const optionManager = new OptionManager(1.2, "bulin");
--- macd-demo-v2/core/timeLineHelper.js
+++ macd-demo-v2/core/timeLineHelper.js
@@ -1,30 +1,150 @@
 class TimeLineHelper {
-    /** helper对应的D3Chart图表示例 */
-    _D3Ins;
-    /** 图表的数据结构信息 */
-    seriesInfo;
-    constructor(chart, index) {
-        this._D3Ins = chart;
-        // 获取helper对应的图表的数据结构
-        this.getSeriesInfo();
-        // 初始化helper
-        this.init();
-        // 通过helper给图表赋最初状态
-        this.initState(index);
-    }
-    getSeriesInfo() {
-        // const D3Ins = this._D3Ins;
-        // D3Ins.getModel().eachComponent('series', model => {
-        //     const view = D3Ins.getViewOfComponentModel(model);
-        //     const grid = model.getAxisModel('y').dependentModels.grid;
-        //     const type = model.type;
+  /** helper对应的D3Chart图表示例 */
+  _D3Ins;
+  /** 图表的数据结构信息 */
+  seriesInfo;
+  constructor(chart, index) {
+    this._D3Ins = chart;
+    this.seriesInfo = [];
+    // 获取helper对应的图表的数据结构
+    this.getSeriesInfo();
+    // 初始化helper
+    this.init();
+    // 通过helper给图表赋最初状态
+    this.initState(index);
+  }
+  getSeriesInfo() {
+    const D3Ins = this._D3Ins;
+    const getInfo = (model) => {
+        const view = D3Ins.getViewOfComponentModel(model);
+        const grid = model.getAxisModel("y").dependentModels.grid;
+        const type = model.type;
+  
+        this.seriesInfo.push({ model, view, grid, type });
+      };
+    D3Ins.getModel().eachComponent("series", getInfo);
+    D3Ins.getModel().eachComponent('markArea',getInfo);
+  }
+  init() {
+    this.seriesInfo.forEach((series) => {
+      if (["bar", "hqbar"].indexOf(series.type) !== -1) {
+        const { model, view, grid } = series;
+        const scale = this._D3Ins.getModel().get('scale');
+        const bandWidth = model.bandwidth;
+        let states = model.points.map((item, index) => {
+          return {
+            // shape: { width: !index ? 0 : model.points[index - 1][0] - bandWidth },
+            shape: { width: item[0] - bandWidth },
+            // lineShape: {width: index ? model.points[index - 1][0] + bandWidth / 2 : bandWidth},
+            // lineShape: {width: item[0] - bandWidth},
+            style: { opacity: 1 },
+          };
+        });
+        let zeroState = {
+          shape: { width: 0 },
+        };
+        const position = grid.position;
+        this.seriesInfo.forEach((itm) => {
+          if (itm.grid.id === grid.id) {
+            if (['custom', 'markArea'].indexOf(itm.type) !== -1) {
+              const elZeroState = {
+                ignore: true,
+              };
+              itm.view.group.childAt(0).eachChild((el, idx) => {
+                const elStates = states.map((item, index) => {
+                  return {
+                    ignore: index < itm.model.get('xIndex'),
+                  };
+                });
+                el.states = elStates;
+                el.zeroState = elZeroState;
+              });
+            } 
+            else {
+              const clipPath = new D3Charts.graphic.Rect({
+                shape: {
+                  x: position.left,
+                  y: position.top,
+                  width: 0,
+                  height: position.height,
+                },
+              });
+              let clipStates;
+              // if (itm.type === 'line') {
+              //   clipStates = states.map(state => {
+              //     return {shape: state.lineShape, style: state.style};
+              //   })
+              // } else {
+                clipStates = states;
+              // }
+              clipPath.states = clipStates;
+              clipPath.zeroState = zeroState;
+              itm.view.group.setClipPath(clipPath);
+            }
+          }
+        });
+      }
+    });
+  }
+  initState(index) {
+    this.seriesInfo.forEach((item) => {
+      const { type, view } = item;
+      if (['custom', 'markArea'].indexOf(type) !== -1) {
+        view.group.childAt(0).eachChild((el) => {
+          this.useState(el, index);
+        });
+      } else {
+        const clipPath = item.view.group.clipPath;
+        if (index === undefined) {
+          this.useState(clipPath, "zeroState");
+        }
+        const idx = Math.max(0, Math.min(index, clipPath.states.length - 1));
+        this.useState(clipPath, idx);
+      }
+    });
+  }
+  useState(el, stateIndex, animation) {
+    const state =
+      stateIndex === "zeroState" ? el[stateIndex] : el.states[stateIndex];
+    animation ? el.animateTo(state) : el.attr(state);
+  }
+  change(index, animation) {
+    this.seriesInfo.forEach((series) => {
+      const { type, view } = series;
+      if (['custom', 'markArea'].indexOf(type) !== -1) {
+        view.group.childAt(0).eachChild((el) => {
+          this.useState(el, index, animation);
+        });
+      } else if (type === 'line') {
+        this.useState(view.group.clipPath, index, true);
+      }
+       else {
+        this.useState(view.group.clipPath, index, animation);
+      }
+    });
+  }
+}
 
-        //     this.seriesInfo.push({
-        //         model, view, grid, type
-        //     })
-        // });
-    }
-    init() {}
-    initState() {}
-
+const changeChartLegend = (chart, lastIndex, changeMap) => {
+  if (!changeMap || !changeMap.length) return;
+  const globalModel = chart.getModel();
+  const legendModel = globalModel.getComponentByIndex('legend', 0);
+  const legendView = chart.getViewOfComponentModel(legendModel);
+  changeMap.forEach(({source, target}) => {
+    const seriesModel = globalModel.getSeriesByName(source)[0];
+    const seriesData = seriesModel.getData();
+    const last = Math.max(0, Math.min(lastIndex, seriesData.length - 1));
+    const name = `{${source}|${target.replace('$', seriesData[last][1].toFixed(2))}}`;
+    legendView.group.eachChild(child => {
+      if (child._name === source) {
+        child.traverse(el => {
+          if (el.style.text){
+            el.attr({
+              style: {text: name}
+            })
+          }
+        })
+      }
+    })
+  })
 }
